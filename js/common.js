9a79c5c3d672c090a9c8463d17434dfe016d3728
oktobat <62067363+oktobat@users.noreply.github.com>
2020-09-10 16:38:22 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -22,7 +22,8 @@
         $('#kimBox').load(url)
     })
 
-    $('.h1Nav > h1 > a').on('click', function(){
+    $('.h1Nav > h1 > a').on('click', function(e){
+        e.preventDefault()
         var url = $(this).attr('href')
         $('#kimContainer').remove()
         $('#kimBox').load(url)
